ed2aa1028d23dd4e211485357e02e5ebd6cc2233
dariokarl16 <dariokarl.sl@gmail.com>
2020-11-07 22:08:55 +0100

    updated gitignore and conda env setup

--- envs/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/dario/Projects/Dario/web-scraping/envs/lib"
-XML2_LIBS="-lxml2 -L/home/dario/Projects/Dario/web-scraping/envs/lib -lz -L/home/dario/Projects/Dario/web-scraping/envs/lib -llzma -lpthread -L/home/dario/Projects/Dario/web-scraping/envs/lib -L/home/dario/Projects/Dario/web-scraping/envs/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/home/dario/Projects/Dario/web-scraping/envs/include/libxml2 -I/home/dario/Projects/Dario/web-scraping/envs/include"
-MODULE_VERSION="xml2-2.9.10"
-
--- envs/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/dario/Projects/Dario/web-scraping/envs/lib"
-XSLT_LIBS="-lxslt -L/home/dario/Projects/Dario/web-scraping/envs/lib -lxml2 -L/home/dario/Projects/Dario/web-scraping/envs/lib -L/home/dario/Projects/Dario/web-scraping/envs/lib -lpthread -L/home/dario/Projects/Dario/web-scraping/envs/lib -L/home/dario/Projects/Dario/web-scraping/envs/lib -licui18n -licuuc -licudata -lm -ldl -lrt"
-XSLT_PRIVATE_LIBS="-lm"
-XSLT_INCLUDEDIR="-I/home/dario/Projects/Dario/web-scraping/envs/include"
-MODULE_VERSION="xslt-1.1.34"
